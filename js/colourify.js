e2f84bde2411ae1da6405763df09a44b99b58f8e
Tyler Chen <chentylerl@gmail.com>
2014-09-19 19:30:10 -0400

    full color support, minor ui changes

--- js/colourify.js
+++ js/colourify.js
@@ -13,56 +13,53 @@
         return m;
     };
 	
-	Colourify.prototype.defaults = {n: 2
-					};
+	Colourify.prototype.defaults = {
+		n: 4,
+		f: 0,
+		color: "r"
+	};
 	
-	Colourify.prototype.options = function( opt ){
-		if( opt !== undefined ){
-			this.options = this.merge_options(this.defaults, opt);
-			console.log( opt );
-			}
-		else {	this.options = this.defaults; console.log( 'options not defined') }
-
-	}
-
-	var func = function(x, y, f_in){ // why can't i get this to work outside of this. how do I use f_in?
-		return Math.round(Math.max(Math.min(
-			0.5*(x+5*y) + 200 //how do i take this from the other place???
-		, 255),0));
-	}
-	
-	// I want to replace the above function with this. I don't know how to convert the string 'f' 
-	// to an expression which I can evaluate
-	// I also can't figure out how to run f_in (from color_finder.r() ) for some reason.
-	Colourify.prototype.f_in = function( f ){ 
-		console.log( f ); 
+	Colourify.prototype.option = function( opt ){
+		this.options = this.merge_options(this.defaults, opt);
 	}
 	
 	//makes useful variables available.
 	//i have no idea if this is a bad way to do this
-	Colourify.prototype.setup = function(){
-		this.n = this.options.n
+	Colourify.prototype.setup = function( f ){
+		this.n = this.options.n 
 		this.dimensions = 100 / this.n + "%"
-		this.step = 255/(this.n - 1);		
+		this.step = 255/(this.n - 1);
+		this.f = f;
+		this.color = this.options.color;
 	}
 	
+		
 	//build grid
 	Colourify.prototype.tabler = function(x,y){ //why do i need x,y?
-		for (y = 0; y < this.options.n; y++) { 
-			for (x = 0; x < this.options.n; x++) {
+		for (y = 0; y < this.n; y++) { 
+			for (x = 0; x < this.n; x++) {
 			//console.log("(" + x +","+ y + ")");
-			this.tiler( Math.round(x * this.step), Math.round(y * this.step) )
+			this.tiler( Math.round(x * this.step), Math.round(y * this.step))
 			}
 		}
 	}
 	
 	//build colour block
-	Colourify.prototype.tiler = function( x , y ){
+	Colourify.prototype.tiler = function( x , y){
 		var div = document.createElement("div");
-		var color = this.color_finder.r(x,y) //need this to be more easily selectable. we can use conditionals based of a value in setup
-		var node = document.createTextNode(color);
+		var c = this.color;
+		var color_finder = this.color_finder
+		var f = this.f
+		
+		var color = function( ){
+			if( c == "r"){return color_finder.r(x,y, f);}
+			else if( c == "g"){return color_finder.g(x,y,f);}
+			else if( c == "b"){return color_finder.b(x,y, f);}
+		}
+			
+		var node = document.createTextNode( color() );
 			div.className = "tile";
-		div.style.background = color;
+		div.style.background = color();
 		div.style.width =  this.dimensions;
 		div.style.height = this.dimensions;
 		//div.appendChild(node); //optionally adds text
@@ -72,16 +69,22 @@
 	}
 	
 	Colourify.prototype.color_finder = {
-		r: function(g, b){
-			var r = func(g, b);
+		r: function(g, b, f){
+			var r = Math.round(Math.max(Math.min(
+						eval( f )  
+					,255),0));
 			return "rgb(" + r +","+ g +","+ b + ")"
 		},
-		g: function(r, b){
-			var g = func(r, b);
+		g: function(r, b, f){
+			var g = Math.round(Math.max(Math.min(
+						eval( f )  
+					,255),0));
 			return "rgb(" + r +","+ g +","+ b + ")";
 		},
-		b: function(r, g){
-			var b = func(r, g);
+		b: function(r, g, f){
+			var b = Math.round(Math.max(Math.min(
+						eval( f )  
+					,255),0));
 			return "rgb(" + r +","+ g +","+ b + ")";
 		}
 	}
@@ -93,10 +96,11 @@
 		}
 	}
 	
-	Colourify.prototype.build = function( f ){ 
-		this.clear()
-		this.f_in( f );
-		this.setup();
+	Colourify.prototype.build = function( opt ){ 
+		
+		this.option( opt );
+		this.clear();
+		this.setup( opt.f );
 		this.tabler();
 	}
 	
